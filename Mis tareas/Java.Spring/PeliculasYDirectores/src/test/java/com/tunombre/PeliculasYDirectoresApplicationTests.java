b474ef91c0b53cae4db3f62d7d07ccd200867e6d
Héctor Fuentes Rebolledo <hfuentesr1@gmail.com>
2024-11-05 21:47:50 -0300

    Add files via upload

---
+++ Mis tareas/Java.Spring/PeliculasYDirectores/src/test/java/com/tunombre/PeliculasYDirectoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tunombre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliculasYDirectoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
